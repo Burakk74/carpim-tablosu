e104c8fd0a21e91e1313a94272db56f63254910f
Burak Küçük <159139651+Burakk74@users.noreply.github.com>
2024-03-31 22:34:42 +0300

    Add files via upload

---
+++ carpimtablosu/src/carpimtablosu/carpimtablosu.java
@@ -0,0 +1,29 @@
+package carpimtablosu;
+
+public class carpimtablosu {
+
+	public static void main(String[] args) {
+		
+	/*	
+		
+		for(int i = 1; i <= 10;i++) {
+			
+			for(int j = 1; j <=  10; j++) {
+				System.out.println(i + "X" + j +"=" + (i*j));
+			}
+			System.out.println("                                                ");
+		}
+*/
+		
+		
+		for(int i = 1 ; i < 11 ; i++) {
+			for(int j=0 ; j<11 ; j++) {
+				System.out.println(i + "x" + j + ":" + (i*j));
+			}
+			System.out.println("                                                                          ");
+		}
+		
+		
+	}
+
+}
---
+++ carpimtablosu/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module carpimtablosu {
+}
